2909288bfdf7acd259694c7c6d1d2b2ad5aa4796
jroca72 <jroca72@gmail.com>
2012-04-03 11:51:33 +0200

    intento de pruebsa en ajax

--- src/Ocaso/OcasoBundle/Controller/SecondoController.php
+++ src/Ocaso/OcasoBundle/Controller/SecondoController.php
@@ -3,6 +3,9 @@
 namespace Ocaso\OcasoBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 
 class SecondoController extends Controller
@@ -13,4 +16,15 @@ class SecondoController extends Controller
         return $this->render('OcasoOcasoBundle:Secondo:secondo.html.twig');
     }
 
+    public function aconaAction()
+    {
+        $request = $this->get('request');
+        if ($request->isXmlHttpRequest()) {
+                return $this->render('OcasoOcasoBundle::trozo.html.twig',array('ponencia' => 'Roca'));
+            }else{
+                $session = $this->get('request')->getSession();
+                $session->setFlash('notice', sprintf("Te has apuntado a quimica"));
+                return $this->redirect($this->generateUrl('Ocaso_principal', array('name' => 'Xabier')));
+            }
+    }
 }
